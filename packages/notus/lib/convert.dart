a5175d8c5eb7b41c2c5834d7c8ea27670955f0fa
Nipuna Sudharaka <nipuna.sudha@gmail.com>
2021-11-07 13:56:55 +0530

    Export notus_html_codec from package

--- packages/notus/lib/convert.dart
+++ packages/notus/lib/convert.dart
@@ -5,9 +5,13 @@
 /// Provides codecs to convert Notus documents to other formats.
 library notus.convert;
 
+import 'package:notus/src/convert/html/notus_html_codec.dart';
+
 import 'src/convert/markdown.dart';
 
+export 'src/convert/html/notus_html_codec.dart';
 export 'src/convert/markdown.dart';
 
 /// Markdown codec for Notus documents.
 const NotusMarkdownCodec notusMarkdown = NotusMarkdownCodec();
+const NotusHtmlCodec notusHtml = NotusHtmlCodec();
